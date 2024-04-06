c91dfa20df8042305727330fe93fe034d42cb380
marissarh <mbutto26@gmail.com>
2024-04-06 16:19:31 -0700

    update POST and DELETE tests

--- daos/items.js
+++ daos/items.js
@@ -11,14 +11,32 @@ module.exports.getAll = () => {
 
 module.exports.getById = (itemId) => {
   // TODO: complete
-}
+  const item = itemsModel.items.find(item => item.id === itemId);
+  return item || null;
+
+};
 
 module.exports.deleteById = async (itemId) => {
     // TODO: complete
-}
+    const index = itemsModel.items.findIndex(item => item.id == itemId);
+    if (index !== -1){
+      itemsModel.items.splice(index, 1);
+    return true;
+  }
+  return false;
+   
+};
 
 module.exports.updateById = async (itemId, newObj) => {
     // TODO: complete
+    const index = itemsModel.items.findIndex(item => item.id === itemId);
+    if(index !== -1){
+      newObj.id = itemId;
+      itemsModel.items[index] = { ...itemsModel.items[index], ...newObj};
+      return true;
+    }
+    return false;
+
 }
 
 module.exports.create = async (item) => {
